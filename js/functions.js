3df651f11562f37bea065f3862a0d4972e551a6e
CendresChaudes <romqaaa1337@gmail.com>
2023-02-20 20:15:51 +0300

    Оптимизирует код функций

--- js/functions.js
+++ js/functions.js
@@ -1,50 +1,26 @@
-function checkLengthString (string, length) {
-  return string.length <= length;
-}
+const checkLengthString = (string, length) => String(string).length <= length;
 
-function checkPalindrome (string) {
-  let uniformString = string.toLowerCase();
+const checkPalindrome = (string) => {
+  string = String(string).toLowerCase().replaceAll(' ', '');
+  return string === string.split('').reverse().join('');
+};
 
-  if (uniformString.indexOf(' ') !== -1) {
-    uniformString = uniformString.replaceAll(' ', '');
+const getInteger = (string) => {
+  string = String(string).replace(/\D/g, '');
+  return parseInt(string, 10);
+};
+
+const addSymbols = (string, length, extension) => {
+  string = String(string);
+  if (string.length >= length) {
+    return string;
   }
 
-  for (let i = 0; i < Math.round(uniformString.length / 2); i++) {
-    if (uniformString[i] !== uniformString[uniformString.length - 1 - i]) {
-      return false;
-    }
+  while (string.length < length) {
+    const extensionLength = length - string.length;
+    string = extension.slice(0, extensionLength) + string;
   }
 
-  return true;
-}
+  return string;
+};
 
-function getInteger (string) {
-  const uniformString = String(string);
-  let integerNumber = '';
-
-  for (const symbol of uniformString) {
-    if (parseInt(symbol, 10) || symbol === '0') {
-      integerNumber += symbol;
-    }
-  }
-
-  return integerNumber ? parseInt(integerNumber, 10) : NaN;
-}
-
-function addSymbols (originalString, minStringLength, additionalSymbols) {
-  if (originalString.length >= minStringLength) {
-    return originalString;
-  }
-
-  let finalString = originalString;
-  while (finalString.length !== minStringLength) {
-    if ((finalString.length + additionalSymbols.length) > minStringLength) {
-      const endSliceValue = minStringLength - finalString.length;
-      const shortedAdditionalSymbols = additionalSymbols.slice(0, endSliceValue);
-      return shortedAdditionalSymbols + finalString;
-    }
-    finalString = additionalSymbols + finalString;
-  }
-
-  return finalString;
-}
